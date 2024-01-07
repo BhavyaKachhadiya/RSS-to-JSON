6da9c26bb3104fad9817441307a3544a952ecab1
BhavyaKachhadiya <kachhadiyabhavya@gmail.com>
2024-01-07 12:42:31 +0530

    added GUI Based API

--- index.cjs
+++ index.cjs
@@ -1,20 +1,25 @@
 const express = require('express');
 const Parser = require('rss-parser'); // Note: Use 'rss-parser' instead of 'parser'
 const cors = require('cors');
+const path = require("path");
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
 const app = express();
 const PORT = 3000;
 
+app.use(express.static(path.join(__dirname,'views')))
 // Middleware to parse JSON
 app.use(express.json());
 app.use(cors());
 
 // API endpoint to convert RSS to JSON
 app.get('/', async (req, res) => {
-  res.send("Vist https://github.com/BhavyaKachhadiya/RSS-to-JSON for documentation")
+  // res.send("Vist  https://github.com/BhavyaKachhadiya/RSS-to-JSON for documentation")
+  res.render('index.html')
 });
 app.get('/api', async (req, res) => {
   try {
-    const url = req.query.url;
+    const url = req.query.url || 'https://dev.to/rss';
     if (!url) {
       return res.status(400).json({ error: 'Missing RSS URL' });
     }
@@ -26,6 +31,47 @@ app.get('/api', async (req, res) => {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
+
+// Swagger configuration
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'RSS to JSON API',
+      version: '1.0.0',
+      description: 'Converts RSS feeds to JSON format.',
+    },
+  },
+  apis: ['index.cjs'],
+};
+
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+
+// API endpoint to convert RSS to JSON
+/**
+ * @openapi
+ * /api:
+ *   get:
+ *     summary: Convert RSS to JSON
+ *     description: Endpoint to convert an RSS feed to JSON format.
+ *     parameters:
+ *       - in: query
+ *         name: url
+ *         required: false
+ *         description: |
+ *           The URL of the RSS feed to convert. If not provided, a default URL will be used.
+ *           Default URL: `https://dev.to/rss`.
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Successful response with JSON representation of the RSS feed.
+ *       400:
+ *         description: Bad request. Missing RSS URL.
+ *       500:
+ *         description: Internal Server Error.
+ */
 
 // Function to parse RSS
 async function parseRSS(url) {
